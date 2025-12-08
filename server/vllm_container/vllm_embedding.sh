bb2c4015627729d21f771b8908d842c26025fafb
Hadi Sheikhi <hadsheikhi@gmail.com>
2025-12-08 23:11:41 +0000

    fix bugs

--- server/vllm_container/vllm_embedding.sh
+++ server/vllm_container/vllm_embedding.sh
@@ -2,6 +2,6 @@
 
 echo $OPENAI_API_KEY
 # now every variable in .env is in your environment
-NVIDIA_VISIBLE_DEVICES=1 vllm serve $EMBEDDER_PATH --served-model-name $EMBEDDER_MODEL_NAME \
+vllm serve $EMBEDDER_PATH --served-model-name $EMBEDDER_MODEL_NAME \
     --dtype auto --api-key $OPENAI_API_KEY  \
     --host 0.0.0.0 --port 25602 --enforce-eager 
--- server/vllm_container/vllm_llm.sh
+++ server/vllm_container/vllm_llm.sh
@@ -3,7 +3,7 @@
 echo $OPENAI_API_KEY
 
 # now every variable in .env is in your environment
-CUDA_VISIBLE_DEVICES=0,1 vllm serve $LLM_PATH --served-model-name $LLM_MODEL_NAME \
+vllm serve $LLM_PATH --served-model-name $LLM_MODEL_NAME \
     --api-key $OPENAI_API_KEY  \
     --host 0.0.0.0 --port 25601 --tensor-parallel-size 2 \
-    --max-model-len 16384
+    --max-model-len 32768
