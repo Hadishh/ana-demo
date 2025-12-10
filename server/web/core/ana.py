af830fd3ff7b4ad4c0bfc84801c2ed86706db949
Hadi Sheikhi <hadsheikhi@gmail.com>
2025-12-10 23:14:30 +0000

    fix bugs

--- server/web/core/ana.py
+++ server/web/core/ana.py
@@ -13,14 +13,14 @@ class ChatBot:
         self.debug_report = str()
         self.persona = f"User's Name: {user.name}\nUser's Age: {user.age}\nUser's City: {user.city}\nCurrent time:{str(datetime.now())}"
 
-
     async def answer(self, dialogue):
         from core.agents import main_agent, graphiti_agent
         from core.function_calls import tool_map, Executor, functions_references
+
         curr_exec = Executor(
-            tool_registry=tool_map, 
-            action_registry={"function_registry": {}}, 
-            funcs_ref=functions_references
+            tool_registry=tool_map,
+            action_registry={"function_registry": {}},
+            funcs_ref=functions_references,
         )
         print(dialogue, f"{self.user.name}: {dialogue[0]['text']}")
         try:
@@ -32,14 +32,17 @@ class ChatBot:
                 reference_time=datetime.now(),
             )
         except Exception as e:
-            print("IGNORING EPISODE: ", f"{self.user.name}: {dialogue[0]['text']}", str(e))
+            print(
+                "IGNORING EPISODE: ", f"{self.user.name}: {dialogue[0]['text']}", str(e)
+            )
 
         response, log_string = await main_agent.generate_functions_and_responses(
-            tool_registry=tool_map, 
-            action_registry={"function_registry": {}}, 
-            persona=self.persona, 
-            dialogue=dialogue, 
-            executor=curr_exec)
+            tool_registry=tool_map,
+            action_registry={"function_registry": {}},
+            persona=self.persona,
+            dialogue=dialogue,
+            executor=curr_exec,
+        )
 
         self.debug_report = log_string
         return response, "other"
